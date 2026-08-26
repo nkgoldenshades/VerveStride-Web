7477e0b39535ba7bc5df4afc072857132fe26f3f
VaultWheels <vaultwheelsin@gmail.com>
2026-08-26 17:24:55 +0530

    Add credit abuse protection - pre-check before AI calls

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1476379065" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2467748823" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
